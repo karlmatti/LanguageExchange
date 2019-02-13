11a6f9bc0d29ee683fb95f31c797ab3048ef1db3
kamatt <kamatt@ttu.ee>
2019-02-13 12:22:02 +0200

    Starting project with Spring Boot 2.1.2 Gradle project with Web, H2, Lombok, Rest Repositories, JPA and Thymeleaf dependencies.

---
+++ src/main/java/language/exchange/langex/LangexApplication.java
@@ -0,0 +1,14 @@
+package language.exchange.langex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LangexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LangexApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/language/exchange/langex/LangexApplicationTests.java
@@ -0,0 +1,17 @@
+package language.exchange.langex;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LangexApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
