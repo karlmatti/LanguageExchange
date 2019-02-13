b1773a76139a286dbbeb1290ccaa15af1a081ece
kamatt <kamatt@ttu.ee>
2019-02-13 15:15:51 +0200

    Integrating Spring Boot with Thymeleaf.

--- src/main/java/language/exchange/langex/LangexApplication.java
+++ src/main/java/language/exchange/langex/LangexApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LangexApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(LangexApplication.class, args);
 	}
 
