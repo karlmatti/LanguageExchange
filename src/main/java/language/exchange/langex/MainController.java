264342431aff5bb6b266cf24e14ef17bd3efa212
kamatt <kamatt@ttu.ee>
2019-02-17 19:39:34 +0200

    A simple SPA with Vue.js and Spring boot completed.

--- src/main/java/language/exchange/langex/MainController.java
+++ src/main/java/language/exchange/langex/MainController.java
@@ -1,14 +1,17 @@
-package LanguageApplication.Spring_Aplication;
+package language.exchange.langex;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MainController {
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("eventName", "FIFA 2018");
+
         return "index";
     }
+    
 }
