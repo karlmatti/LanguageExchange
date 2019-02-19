56be42a18bcaa9c038ea5cfd9fc599335f7862db
kamatt <kamatt@ttu.ee>
2019-02-19 17:31:16 +0200

    Removed build2.gradle file.

---
+++ src/main/java/language/exchange/langex/HomeController.java
@@ -0,0 +1,14 @@
+package language.exchange.langex;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = "/")
+    public String index() {
+        return "index";
+    }
+
+}
