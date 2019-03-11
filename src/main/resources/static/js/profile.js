8f972f67a19c6a230c3c33331acf4709a4dcad12
Igor Kozlovski <igkozl@ttu.ee>
2019-03-11 16:34:36 +0200

    Vue displays data from database

--- src/main/resources/static/js/profile.js
+++ src/main/resources/static/js/profile.js
@@ -3,9 +3,9 @@ var temp = $("#googleID").text();
 console.log(temp);
 var userApi = Vue.resource('/users{/id}');
 
-Vue.component('message-row', {
+/*Vue.component('message-row', {
     props: ['finalMessage'],
-    template: '<div>{{finalMessage.firstName}}</div>'
+    template: '<div>{{finalMessage}}</div>'
 });
 
 Vue.component('messages-list', {
@@ -15,25 +15,20 @@ Vue.component('messages-list', {
         '<message-row v-for="message in get" v-bind:finalMessage="message" :key="message.id"></message-row>' +
         '</div>'
 
-});
+});*/
 
 var app = new Vue ({
     el: '#pData',
-    template: '<messages-list v-bind:get="messages" ></messages-list>',
     data: {
-        messages: []
+        messages: ''
     },
     created: function () {
         userApi.get().then(result =>
         result.json().then(data =>
             data.forEach(user => {
                 if (user.googleId == temp) {
-                return this.messages.push(user);
+                this.messages = user;
         }}))
     )},
-    mounted: function() {
-        console.log("bla");
-        console.log(this.$refs.testElement);
-    }
 });
 console.log("right right right");
