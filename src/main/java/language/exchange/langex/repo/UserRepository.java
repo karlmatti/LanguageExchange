95b473a8cb1ca004afd45bbe60204a31baca0f4c
Igor Kozlovski <igkozl@ttu.ee>
2019-03-13 20:33:44 +0200

    fixed problems with style which Rasmus has created :D

--- src/main/java/language/exchange/langex/repo/UserRepository.java
+++ src/main/java/language/exchange/langex/repo/UserRepository.java
@@ -3,5 +3,4 @@ package language.exchange.langex.repo;
 import language.exchange.langex.model.User;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository<User, Integer> {
-}
+public interface UserRepository extends CrudRepository<User, Integer> {}
--- src/main/resources/static/js/profile.js
+++ src/main/resources/static/js/profile.js
@@ -26,40 +26,11 @@ var app = new Vue ({
         userApi.get().then(result =>
         result.json().then(data =>
             data.forEach(user => {
-                if (user.googleId == temp) {
+                if (user.id == temp) {
+                    console.log('some')
+                    console.log("console");
                 this.messages = user;
         }}))
     )},
 });
 console.log("right right right");
-
-new Vue({
-    el: '#profilePicture',
-    data: {
-        items: [
-            {
-                image: false,
-            },
-        ],
-    },
-    methods: {
-        onFileChange(item, e) {
-            var files = e.target.files || e.dataTransfer.files;
-            if (!files.length)
-                return;
-            this.createImage(item, files[0]);
-        },
-        createImage(item, file) {
-            var image = new Image();
-            var reader = new FileReader();
-
-            reader.onload = (e) => {
-                item.image = e.target.result;
-            };
-            reader.readAsDataURL(file);
-        },
-        removeImage: function (item) {
-            item.image = false;
-        }
-    }
-})
