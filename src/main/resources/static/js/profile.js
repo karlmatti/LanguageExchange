5bbe90a5bf3fa1b8a185860c6338d5563e740a62
Rasmus Rugam <Rugam.rasmus@gmail.com>
2019-03-13 14:12:05 +0200

    Prototype final stage?!? added profile picture upload, but not connected with database

--- src/main/resources/static/js/profile.js
+++ src/main/resources/static/js/profile.js
@@ -32,3 +32,34 @@ var app = new Vue ({
     )},
 });
 console.log("right right right");
+
+new Vue({
+    el: '#profilePicture',
+    data: {
+        items: [
+            {
+                image: false,
+            },
+        ],
+    },
+    methods: {
+        onFileChange(item, e) {
+            var files = e.target.files || e.dataTransfer.files;
+            if (!files.length)
+                return;
+            this.createImage(item, files[0]);
+        },
+        createImage(item, file) {
+            var image = new Image();
+            var reader = new FileReader();
+
+            reader.onload = (e) => {
+                item.image = e.target.result;
+            };
+            reader.readAsDataURL(file);
+        },
+        removeImage: function (item) {
+            item.image = false;
+        }
+    }
+})
